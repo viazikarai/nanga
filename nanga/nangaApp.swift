84c4070a5f8c213fba3af28d67348acc63d0f561
viazikarai <janiceink001@gmail.com>
2026-04-06 16:30:21 +0300

    Initial Commit

---
+++ nanga/nangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  nangaApp.swift
+//  nanga
+//
+//  Created by Nawal 🫧💗🛼 on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct nangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nangaTests/nangaTests.swift
@@ -0,0 +1,17 @@
+//
+//  nangaTests.swift
+//  nangaTests
+//
+//  Created by Nawal 🫧💗🛼 on 06/04/2026.
+//
+
+import Testing
+@testable import nanga
+
+struct nangaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
